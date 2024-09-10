28ce4e272de8341ad1ed374e707bca292aec5c9b
Jadon Narendrasinh M <narendrarajput05007@gmail.com>
2024-09-10 18:57:02 +0530

    commit

---
+++ controller/skillController.js
@@ -0,0 +1,94 @@
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
+import ErrorHandler from "../middlewares/error.js";
+import { Skill } from "../models/skillSchema.js";
+import { v2 as cloudinary } from "cloudinary";
+
+export const addNewSkill = catchAsyncErrors(async (req, res, next) => {
+  const { title, proficiency } = req.body;
+  const svg = req.files?.svg;
+
+  if (!svg) {
+    return next(new ErrorHandler("Image For Skill Required!", 400));
+  }
+
+  if (!title || !proficiency) {
+    return next(new ErrorHandler("Please provide title and proficiency", 400));
+  }
+
+  try {
+    const cloudinaryResponse = await cloudinary.uploader.upload(svg.tempFilePath, {
+      folder: "PORTFOLIO SKILL IMAGES"
+    });
+
+    const skill = await Skill.create({
+      title,
+      proficiency,
+      svg: {
+        public_id: cloudinaryResponse.public_id,
+        url: cloudinaryResponse.secure_url,
+      },
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "New Skill Added",
+      skill,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    return next(new ErrorHandler("Failed to add new skill", 500));
+  }
+});
+
+export const deleteSkill = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+  
+  const skill = await Skill.findById(id);
+  if (!skill) {
+    return next(new ErrorHandler("Skill not found", 404));
+  }
+
+  try {
+    await cloudinary.uploader.destroy(skill.svg.public_id);
+    await skill.deleteOne();
+
+    res.status(200).json({
+      success: true,
+      message: "Skill Deleted!",
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    return next(new ErrorHandler("Failed to delete skill", 500));
+  }
+});
+export const updateSkill = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+  let skill = await Skill.findById(id);
+  if (!skill) {
+    return next(new ErrorHandler("Skill not found!", 404));
+  }
+  const { proficiency } = req.body;
+  skill = await Skill.findByIdAndUpdate(
+    id,
+    { proficiency },
+    {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    }
+  );
+  res.status(200).json({
+    success: true,
+    message: "Skill Updated!",
+    skill,
+  });
+});
+
+export const getAllSkills = catchAsyncErrors(async (req, res, next) => {
+  const skills = await Skill.find();
+  res.status(200).json({
+    success: true,
+    skills,
+  });
+});
+
